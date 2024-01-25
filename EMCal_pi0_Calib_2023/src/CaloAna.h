de8086caadf3fee383442529e347db653a0a070e
Nikhil Kumar <137448289+nk7252@users.noreply.github.com>
2024-01-25 15:49:25 -0500

    Update CaloAna.h

--- EMCal_pi0_Calib_2023/src/CaloAna.h
+++ EMCal_pi0_Calib_2023/src/CaloAna.h
@@ -66,6 +66,9 @@ class CaloAna : public SubsysReco
   TH1F* h_Dphidist_InvMass_over200M = nullptr;
   TH1F* h_phidist_InvMass_under200M = nullptr;
   TH1F* h_phidist_InvMass_over200M = nullptr;
+
+  TH1F* h_Detadist_InvMass_under200M = nullptr;
+  TH1F* h_Detadist_InvMass_over200M = nullptr;
   // should add truth histos too?
 
   TH2F* h_cemc_etaphi = nullptr;
