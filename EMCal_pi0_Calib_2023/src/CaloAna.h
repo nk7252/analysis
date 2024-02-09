ddc2b0422707b085bdfb2b0269af9d776f75da73
Nikhil Kumar <137448289+nk7252@users.noreply.github.com>
2024-02-09 12:06:46 -0500

    change weight to double. it had integer overflow. do I even need it anymore?

--- EMCal_pi0_Calib_2023/src/CaloAna.h
+++ EMCal_pi0_Calib_2023/src/CaloAna.h
@@ -182,7 +182,7 @@ class CaloAna : public SubsysReco
   TH1F* h_truth_pid_cuts[6];
 
   float target_pi0_mass = 0.145;
-  int WeightScale=1e+14;
+  double WeightScale=1e+14;
   double inv_yield;
 
 };
