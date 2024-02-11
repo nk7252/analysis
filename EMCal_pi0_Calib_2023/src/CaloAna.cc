ffb3164227d1ac53aaaa61e2a52a3f46a8e953f1
Nikhil Kumar <nkumar7252@gmail.com>
2024-02-11 00:17:44 -0500

    Update CaloAna.cc

--- EMCal_pi0_Calib_2023/src/CaloAna.cc
+++ EMCal_pi0_Calib_2023/src/CaloAna.cc
@@ -807,18 +807,18 @@ int CaloAna::process_towers(PHCompositeNode* topNode)
       pi0gammavec[2]=pi0;//pion
 
       for(size_t i=0; i<badcalibsmearint.size(); i++){
-        std::cout << "smear " << i << "start" <<std::endl;
+        //std::cout << "smear " << i << "start" <<std::endl;
         double smear1=( ( generateRandomNumber()*badcalibsmear[i] ) + 1 );
         double smear2=( ( generateRandomNumber()*badcalibsmear[i] ) + 1 );
         pi0smearvec[0][i]= photon1*smear1;
         pi0smearvec[1][i]= photon2*smear2;
         pi0smearvec[2][i]= pi0smearvec[0][i]+pi0smearvec[1][i];
-        std::cout << "smear " << i << "end" <<std::endl;
+        //std::cout << "smear " << i << "end" <<std::endl;
         if((pi0smearvec[0][i].Pt() > pt1ClusCut || pi0smearvec[0][i].Pt() < ptMaxCut) && (pi0smearvec[1][i].Pt() > pt2ClusCut || pi0smearvec[1][i].Pt() < ptMaxCut) && fabs(pi0smearvec[0][i].E() - pi0smearvec[1][i].E()) / (pi0smearvec[0][i].E() + pi0smearvec[1][i].E()) < maxAlpha && pi0smearvec[0][i].DeltaR(pi0smearvec[1][i]) < maxDr && pi0smearvec[2][i].Pt() > pi0ptcut){
-          std::cout << "cuts passed for smear" << i <<std::endl;
+          std::cout << "cuts passed for smear " << i <<std::endl;
           h_InvMass_badcalib_smear[i]->Fill(pi0smearvec[2][i].M());
           h_InvMass_badcalib_smear_weighted[i]->Fill(pi0smearvec[2][i].M(), inv_yield);
-          std::cout << "histograms filled" << i <<std::endl;
+          std::cout << "histograms filled for " << i <<std::endl;
         }
       }
 
