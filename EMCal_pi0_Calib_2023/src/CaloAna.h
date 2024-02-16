58157a281bda8d03d7a116dc569a7743735eb552
Nikhil Kumar <nkumar7252@gmail.com>
2024-02-16 00:11:48 -0500

    Update CaloAna.h

--- EMCal_pi0_Calib_2023/src/CaloAna.h
+++ EMCal_pi0_Calib_2023/src/CaloAna.h
@@ -156,7 +156,7 @@ class CaloAna : public SubsysReco
   std::vector<int> m_bbc_side;
 
   float badcalibsmear;
-  int badcalibsmearint=19;
+  int badcalibsmearint=15;
   std::vector<float> pidcuts;
 
 
