313b5d6c45cb50aad9db5cf0ad029d2fcc4a4c2a
Nikhil Kumar <nkumar7252@gmail.com>
2024-02-25 17:49:20 -0500

    Update CaloAna.h

--- EMCal_pi0_Calib_2023/src/CaloAna.h
+++ EMCal_pi0_Calib_2023/src/CaloAna.h
@@ -166,7 +166,7 @@ class CaloAna : public SubsysReco
   bool additionalsmearing = true;
   bool cutson = true;
   bool poscor = true;
-  std::string clustposcorstring;
+  char* clustposcorstring;
 
   int _range = 1;
   float _vz = 0.;
