60b7a9be2a747bddbd9d840c5a3f331bbf2da790
Nikhil Kumar <nkumar7252@gmail.com>
2024-06-10 21:12:04 -0400

    Update cleanup_script.sh

--- EMCal_pi0_Calib_2023/macros/dynamic_condor/cleanup_script.sh
+++ EMCal_pi0_Calib_2023/macros/dynamic_condor/cleanup_script.sh
@@ -6,6 +6,11 @@ export LOGNAME=${USER}
 export HOME=/sphenix/u/${LOGNAME}
 baseDir=${HOME}/analysis/EMCal_pi0_calib_2023/macros/dynamic_condor
 
+#always run this setup anyway
+source /opt/sphenix/core/bin/sphenix_setup.sh -n new
+export MYINSTALL=/sphenix/u/nkumar/Install
+source /opt/sphenix/core/bin/setup_local.sh $MYINSTALL 
+
 # Run the hadd command to merge ROOT files, removed ./ from both
 hadd -f "${baseDir}/output/merged_file.root" ${baseDir}/condorout/OutDir*/*.root
 
