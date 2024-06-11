1b460a8665752eb625730a50b51dbe1ac3cf249b
Nikhil Kumar <137448289+nk7252@users.noreply.github.com>
2024-06-11 15:13:45 -0400

    Update Dynamic_job.sh

--- EMCal_pi0_Calib_2023/macros/dynamic_condor/Dynamic_job.sh
+++ EMCal_pi0_Calib_2023/macros/dynamic_condor/Dynamic_job.sh
@@ -165,4 +165,4 @@ echo "JOB CLEANUP cleanup.sub" >> $dagfile
 echo "PARENT $(seq -s ' ' -f "Job%.0f" 0 $((njob - 1))) CHILD CLEANUP" >> $dagfile
 
 # Submit the DAG file
-condor_submit_dag -f -debug 3 -no_submit $dagfile
+condor_submit_dag -f -debug 3 $dagfile
