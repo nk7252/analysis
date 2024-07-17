ba88bf8f60d89960cd2b8b1fcd8fbf97833ce771
Nikhil Kumar <nkumar7252@gmail.com>
2024-07-17 00:47:09 -0400

    Update bgsub.C

--- EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
+++ EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
@@ -324,7 +324,7 @@ void fit_2d_histogram(Double_t scale_factor, std::vector<float> &limits, bool fi
 {
   // more thorough minimizer for fit
 
-  // ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2");  //,"Simplex","Fumili"
+  ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2");  //,"Simplex","Fumili"
   //  Set the global fit strategy
   ROOT::Math::MinimizerOptions::SetDefaultStrategy(2);
   ROOT::Math::MinimizerOptions::SetDefaultMaxFunctionCalls(10000);
