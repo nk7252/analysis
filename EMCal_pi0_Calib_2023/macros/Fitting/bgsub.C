03484cc1b60602cd82c7b899da20169698226553
Nikhil Kumar <nkumar7252@gmail.com>
2024-07-17 01:37:53 -0400

    Update bgsub.C

--- EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
+++ EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
@@ -351,6 +351,7 @@ void fit_2d_histogram(Double_t scale_factor, std::vector<float> &limits, bool fi
   ROOT::Math::MinimizerOptions::SetDefaultStrategy(2);
   ROOT::Math::MinimizerOptions::SetDefaultMaxFunctionCalls(1000000);
   ROOT::Math::MinimizerOptions::SetDefaultMaxIterations(10000);
+  ROOT::Math::MinimizerOptions::SetDefaultTolerance(0.001);
   SetsPhenixStyle();
 
   // Ensure the limits vector has the correct size
