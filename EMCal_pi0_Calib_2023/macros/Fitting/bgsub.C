c31356543cd3635cf51fb066022fe0e1340c6971
Nikhil Kumar <nkumar7252@gmail.com>
2024-07-17 02:33:02 -0400

    Update bgsub.C

--- EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
+++ EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
@@ -346,7 +346,7 @@ void fit_2d_histogram(Double_t scale_factor, std::vector<float> &limits, bool fi
 {
   // more thorough minimizer for fit
 
-  ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2", "Fumili2");  //,"Simplex", "Migrad"
+  //ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2", "Fumili2");  //,"Simplex", "Migrad"
   //ROOT::Math::MinimizerOptions::SetDefaultStrategy(2);
   //ROOT::Math::MinimizerOptions::SetDefaultMinimizer("GSLMultiMin");//, "ConjugateFR"
   //ROOT::Math::MinimizerOptions::SetDefaultMinimizer("GSLMultiFit", "LevenbergMarquardt");
