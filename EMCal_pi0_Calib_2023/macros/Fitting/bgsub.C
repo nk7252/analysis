0a27b88a1643a6325d706d33e9f75f97a53ea4c5
Nikhil Kumar <nkumar7252@gmail.com>
2024-07-17 08:17:42 -0400

    Update bgsub.C

--- EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
+++ EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
@@ -348,8 +348,8 @@ void fit_2d_histogram(Double_t scale_factor, std::vector<float> &limits, bool fi
 
   //ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2", "Fumili2");  //,"Simplex", "Migrad"
   //ROOT::Math::MinimizerOptions::SetDefaultStrategy(2);
-  ROOT::Math::MinimizerOptions::SetDefaultMinimizer("GSLMultiMin");//, "ConjugateFR"
-  //ROOT::Math::MinimizerOptions::SetDefaultMinimizer("GSLMultiFit", "LevenbergMarquardt");
+  //ROOT::Math::MinimizerOptions::SetDefaultMinimizer("GSLMultiMin");//, "ConjugateFR"
+  ROOT::Math::MinimizerOptions::SetDefaultMinimizer("GSLMultiFit", "LevenbergMarquardt");
   //ROOT::Math::MinimizerOptions::SetDefaultPrintLevel(3);
   //  Set the global fit strategy
   ROOT::Math::MinimizerOptions::SetDefaultStrategy(2);
@@ -542,7 +542,7 @@ void fit_2d_histogram(Double_t scale_factor, std::vector<float> &limits, bool fi
 
     // Fit second Gaussian in the specified range
     TF1 *gausFit2 = new TF1("gausFit2", "gaus", limits[6], limits[7]);
-    gausFit2->SetParLimits(1, 0.55, 0.64);
+    gausFit2->SetParLimits(1, 0.55, 0.63);
     gausFit2->SetParLimits(2, 0.03, 0.25);
     histF->Fit(gausFit2, "R");
     if (fitEtaPeak)
@@ -577,35 +577,35 @@ void fit_2d_histogram(Double_t scale_factor, std::vector<float> &limits, bool fi
           combinedFit->SetParameter(j + 3, gausFit2->GetParameter(j));
         }
         combinedFit->SetParLimits(3, 0, gausFit2->GetParameter(0) * 1.05);  // gausFit2->GetParameter(0) *0.5
-        combinedFit->SetParLimits(4, 0.5, 0.64);
+        combinedFit->SetParLimits(4, 0.5, 0.63);
         combinedFit->SetParLimits(5, 0.03, 0.25);
       }
       else if (background_scheme == 2)
       {
         for (int j = 0; j < 2; ++j) combinedFit->SetParameter(j + 6, leftRightFit->GetParameter(j));
         for (int j = 0; j < 3; ++j) combinedFit->SetParameter(j + 3, gausFit2->GetParameter(j));
-        combinedFit->SetParLimits(4, 0.55, 0.64);
+        combinedFit->SetParLimits(4, 0.55, 0.63);
         combinedFit->SetParLimits(5, 0.03, 0.25);
       }
       else if (background_scheme == 3)
       {
         for (int j = 0; j < 3; ++j) combinedFit->SetParameter(j + 6, leftRightFit->GetParameter(j));
         for (int j = 0; j < 3; ++j) combinedFit->SetParameter(j + 3, gausFit2->GetParameter(j));
-        combinedFit->SetParLimits(4, 0.55, 0.64);
+        combinedFit->SetParLimits(4, 0.55, 0.63);
         combinedFit->SetParLimits(5, 0.03, 0.25);
       }
       else if (background_scheme == 4)
       {
         for (int j = 0; j < 4; ++j) combinedFit->SetParameter(j + 6, leftRightFit->GetParameter(j));
         for (int j = 0; j < 3; ++j) combinedFit->SetParameter(j + 3, gausFit2->GetParameter(j));
-        combinedFit->SetParLimits(4, 0.55, 0.64);
+        combinedFit->SetParLimits(4, 0.55, 0.63);
         combinedFit->SetParLimits(5, 0.03, 0.25);
       }
       else if (background_scheme == 5)
       {
         for (int j = 0; j < 6; ++j) combinedFit->SetParameter(j + 6, leftRightFit->GetParameter(j));
         for (int j = 0; j < 3; ++j) combinedFit->SetParameter(j + 3, gausFit2->GetParameter(j));
-        combinedFit->SetParLimits(4, 0.55, 0.64);
+        combinedFit->SetParLimits(4, 0.55, 0.63);
         combinedFit->SetParLimits(5, 0.03, 0.25);
         combinedFit->SetParLimits(3, 0, gausFit2->GetParameter(0) * 1.05);  // gausFit2->GetParameter(0) *0.5
       }
@@ -613,7 +613,7 @@ void fit_2d_histogram(Double_t scale_factor, std::vector<float> &limits, bool fi
       {
         for (int j = 0; j < 7; ++j) combinedFit->SetParameter(j + 6, leftRightFit->GetParameter(j));
         for (int j = 0; j < 3; ++j) combinedFit->SetParameter(j + 3, gausFit2->GetParameter(j));
-        combinedFit->SetParLimits(4, 0.55, 0.64);
+        combinedFit->SetParLimits(4, 0.55, 0.63);
         combinedFit->SetParLimits(5, 0.03, 0.25);
         combinedFit->SetParLimits(3, 0, gausFit2->GetParameter(0) * 1.05);  // gausFit2->GetParameter(0) *0.5
       }
