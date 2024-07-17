5d4e108b6bcb672f63c7d80c5b67b84f9e3f8908
Nikhil Kumar <nkumar7252@gmail.com>
2024-07-17 18:07:18 -0400

    Update Fun4All_EMCal_sp.C

--- EMCal_pi0_Calib_2023/macros/Fun4All_EMCal_sp.C
+++ EMCal_pi0_Calib_2023/macros/Fun4All_EMCal_sp.C
@@ -1,6 +1,11 @@
-#include <caloreco/RawClusterPositionCorrection.h>
+#ifndef FUN4ALL_YEAR1_C
+#define FUN4ALL_YEAR1_C
+
 #include <caloreco/CaloTowerCalib.h>
 #include <caloreco/RawClusterBuilderTemplate.h>
+#include <caloreco/RawClusterDeadHotMask.h>
+#include <caloreco/RawClusterPositionCorrection.h>
+#include <caloreco/TowerInfoDeadHotMask.h>
 
 #include <ffamodules/FlagHandler.h>
 #include <ffamodules/HeadReco.h>
@@ -16,11 +21,16 @@
 #include <fun4all/Fun4AllUtils.h>
 #include <fun4all/SubsysReco.h>
 
+#include <globalvertex/GlobalVertexReco.h>
+#include <mbd/MbdReco.h>
+
 #include <phool/recoConsts.h>
 
+#include <cdbobjects/CDBTTree.h>  // for CDBTTree
 #include <ffamodules/CDBInterface.h>
 #include <GlobalVariables.C>
 
+#include <litecaloeval/LiteCaloEval.h>
 
 R__LOAD_LIBRARY(libcdbobjects)
 
@@ -28,12 +38,25 @@ R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libfun4allraw.so)
 R__LOAD_LIBRARY(libcalo_reco.so)
 R__LOAD_LIBRARY(libffamodules.so)
+R__LOAD_LIBRARY(libmbd.so)
+R__LOAD_LIBRARY(libglobalvertex.so)
+R__LOAD_LIBRARY(libLiteCaloEvalTowSlope.so)
 
 #include <caloana/CaloAna.h>
 R__LOAD_LIBRARY(libcaloana.so)
 
+void createLocalEMCalCalibFile(const string fname, int runNumber);
+
+
+
+
+//void Fun4All_EMCal(int nevents = 0, const std::string &fname = "inputdata.txt",int iter = 0, const std::string &calib_fname="local_calib_copy.root")
 void Fun4All_EMCal_sp(int nevents = 10000, const std::string &fname = "inputdata_sp.txt", const std::string &fname_truth = "g4hits.list")
 {
+  bool enableMasking = 0;
+
+  bool doFit = 0;
+  bool doHistMake = 1;
 
   Fun4AllServer *se = Fun4AllServer::instance();
   se->Verbosity(0);
@@ -57,15 +80,16 @@ void Fun4All_EMCal_sp(int nevents = 10000, const std::string &fname = "inputdata
   rc->set_StringFlag("CDB_GLOBALTAG", "MDC2");
   // // 64 bit timestamp
   rc->set_uint64Flag("TIMESTAMP", runnumber);
-
+  cout << "tag, timestamp done" << endl;
   Fun4AllInputManager *in = new Fun4AllDstInputManager("DST_TOWERS");
   Fun4AllInputManager *intruth = new Fun4AllDstInputManager("DST_TRUTH");
-
+  cout << "add listfiles to input manager" << endl;
   in->AddListFile(fname);
   intruth->AddListFile(fname_truth);
-
+  cout << "files added" << endl;
   se->registerInputManager(in);
   se->registerInputManager(intruth);
+  cout << "input manager registered" << endl;
 
   //Fun4AllInputManager *in2 = new Fun4AllDstInputManager("DST_TOWERS2");
   //in2->AddListFile(fnamehits);
@@ -73,20 +97,65 @@ void Fun4All_EMCal_sp(int nevents = 10000, const std::string &fname = "inputdata
 
   std::string filename = first_file.substr(first_file.find_last_of("/\\") + 1);
   std::string OutFile = Form("OUTHIST_iter_%s",filename.c_str());
+  //std::string OutFile = Form("OUTHIST_iter%d_%s",iter , filename.c_str());
+  
+  /*
+  if (iter == 0)
+  {
+    cout << "creating emcal calib" << endl;
+    createLocalEMCalCalibFile(calib_fname.c_str(), runnumber);
+    cout << "creating " << calib_fname.c_str() << " and exiting" << endl;
+    return;
+  }
+
+  ////////////////////
+  // Calibrate towers
+  
+  std::cout << "Calibrating EMCal" << std::endl;
+  CaloTowerCalib *calibEMC = new CaloTowerCalib("CEMCCALIB");
+  calibEMC->set_detector_type(CaloTowerDefs::CEMC);
+  calibEMC->set_directURL(calib_fname.c_str());
+  se->registerSubsystem(calibEMC);
+
+
+  //////////////////
+  // Clusters
+  std::cout << "Building clusters" << std::endl;
+  RawClusterBuilderTemplate *ClusterBuilder = new RawClusterBuilderTemplate("EmcRawClusterBuilderTemplate");
+  ClusterBuilder->Detector("CEMC");
+  ClusterBuilder->set_threshold_energy(0.20);  // for when using basic calibration
+  std::string emc_prof = getenv("CALIBRATIONROOT");
+  emc_prof += "/EmcProfile/CEMCprof_Thresh30MeV.root";
+  ClusterBuilder->LoadProfile(emc_prof);
+  ClusterBuilder->set_UseTowerInfo(1);  // to use towerinfo objects rather than old RawTower
+  ClusterBuilder->setOutputClusterNodeName("CLUSTERINFO_CEMC2");
+  se->registerSubsystem(ClusterBuilder);
+  */
 /*
   std::cout << "Applying Position Dependent Correction" << std::endl;
   RawClusterPositionCorrection *clusterCorrection = new RawClusterPositionCorrection("CEMC");
-  //clusterCorrection->set_UseTowerInfo(1);  
+  clusterCorrection->set_UseTowerInfo(1);  // to use towerinfo objects rather than old RawTower  
   se->registerSubsystem(clusterCorrection);
 */
 
   ///////////////////
   // analysis modules
+    /*
+    if (iter==1){
+    LiteCaloEval *eval7e = new LiteCaloEval("CEMCEVALUATOR2", "CEMC",OutFile);
+    eval7e->CaloType(LiteCaloEval::CEMC);
+    eval7e->setInputTowerNodeName("TOWERINFO_CALIB_CEMC");
+    se->registerSubsystem(eval7e);
+  }
+  */
+
+  //if (iter>1){
   CaloAna *ca = new CaloAna("calomodulename", OutFile);
   ca->set_timing_cut_width(16);
   ca->apply_vertex_cut(false);
   ca->set_vertex_cut(20.);
   se->registerSubsystem(ca);
+  //}
 
   se->run(nevents);
   se->End();
@@ -96,6 +165,46 @@ void Fun4All_EMCal_sp(int nevents = 10000, const std::string &fname = "inputdata
   TFile* f_done_signal = new TFile("DONE.root","recreate");
   std::cout << "All done!" << std::endl;
   gSystem->Exit(0);
-
 }
 
+
+
+
+void createLocalEMCalCalibFile(const string fname, int runNumber)
+{
+  string default_time_independent_calib = "cemc_pi0_twrSlope_v1_default";
+  string m_calibName = "cemc_pi0_twrSlope_v1";
+  std::cout << "calib:GET URL" << std::endl;
+  string calibdir = CDBInterface::instance()->getUrl(m_calibName);
+  string filePath;
+  std::cout << "calib:check calibdir" << std::endl;
+  if (!calibdir.empty())
+  {
+    std::cout << "calib:calibdir not empty" << std::endl;
+    filePath = calibdir;
+    // cdbttree = new CDBTTree(calibdir);
+  }
+  else
+  {
+    std::cout << "calib:Calibdir maybe empty" << std::endl;
+    calibdir = CDBInterface::instance()->getUrl(default_time_independent_calib);
+
+    if (calibdir.empty())
+    {
+      std::cout << "No EMCal Calibration NOT even a default" << std::endl;
+      exit(1);
+    }
+    filePath = calibdir;
+    // cdbttree = new CDBTTree(calibdir);
+    std::cout << "No specific file for " << m_calibName << " found, using default calib " << default_time_independent_calib << std::endl;
+  }
+
+  TFile *f_cdb = new TFile(filePath.c_str());
+  f_cdb->Cp(fname.c_str());
+
+  std::cout << "created local Calib file for run " << runNumber << " named " << fname << std::endl;
+
+  delete f_cdb;
+}
+
+#endif
