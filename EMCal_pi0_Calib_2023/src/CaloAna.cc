5dd594ebbaf74cc19e8e9ed7752f35fd0eb7f428
Nikhil Kumar <137448289+nk7252@users.noreply.github.com>
2024-07-17 13:53:41 -0400

    Update CaloAna.cc

--- EMCal_pi0_Calib_2023/src/CaloAna.cc
+++ EMCal_pi0_Calib_2023/src/CaloAna.cc
@@ -613,7 +613,7 @@ int CaloAna::process_towers(PHCompositeNode* topNode)
             truthpi0.SetPtEtaPhiE(pion_pt, pion_eta, pion_phi, pion_e);
             float delR = pi0.DeltaR(truthpi0);
             //h_delR_recTrth->Fill(delR);
-            if (delR < 0.015)
+            if (delR < 0.3)
             { 
               truth_pions.push_back(truthpi0);
               h_truthmatched_mass->Fill(pi0.M());
