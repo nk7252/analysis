b0490e532786eb256af3ce823c570932c77c2cad
Nikhil Kumar <nkumar7252@gmail.com>
2024-07-22 04:01:42 -0400

    Update bgsub.C

--- EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
+++ EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
@@ -380,17 +380,17 @@ void fit_2d_histogram(Double_t scale_factor, std::vector<float> &limits, bool fi
 {
   // more thorough minimizer for fit
 
-  ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2", "Fumili2");  //,"Simplex", "Migrad", "Combined"
+  //ROOT::Math::MinimizerOptions::SetDefaultMinimizer("Minuit2", "Fumili2");  //,"Simplex", "Migrad", "Combined"
   // ROOT::Math::MinimizerOptions::SetDefaultStrategy(2);
   // ROOT::Math::MinimizerOptions::SetDefaultMinimizer("GSLMultiMin");//, "ConjugateFR"
-  //ROOT::Math::MinimizerOptions::SetDefaultMinimizer("GSLMultiFit", "LevenbergMarquardt");
-  // ROOT::Math::MinimizerOptions::SetDefaultPrintLevel(3);
+  ROOT::Math::MinimizerOptions::SetDefaultMinimizer("GSLMultiFit", "LevenbergMarquardt");
+  //ROOT::Math::MinimizerOptions::SetDefaultPrintLevel(3);
   //   Set the global fit strategy
   ROOT::Math::MinimizerOptions::SetDefaultStrategy(2);
   ROOT::Math::MinimizerOptions::SetDefaultMaxFunctionCalls(1000000);
   ROOT::Math::MinimizerOptions::SetDefaultMaxIterations(10000);
-  //ROOT::Math::MinimizerOptions::SetDefaultTolerance(0.0001);
-  //ROOT::Math::MinimizerOptions::SetDefaultPrecision(1e-8);
+  ROOT::Math::MinimizerOptions::SetDefaultTolerance(0.0001);
+  ROOT::Math::MinimizerOptions::SetDefaultPrecision(1e-8);
   SetsPhenixStyle();
 
   // Ensure the limits vector has the correct size
