84e6ccb1425164a691d8aad2560df3dd37eefd23
Nikhil Kumar <nkumar7252@gmail.com>
2024-07-22 03:32:23 -0400

    Update bgsub.C

--- EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
+++ EMCal_pi0_Calib_2023/macros/Fitting/bgsub.C
@@ -664,7 +664,7 @@ void fit_2d_histogram(Double_t scale_factor, std::vector<float> &limits, bool fi
 
     // Fit the combined function
     combinedFit->SetNpx(1000);
-    histF->Fit(combinedFit, "RE");
+    histF->Fit(combinedFit, "REI");
     
     // After fitting
     std::cout << "Background only Fit Parameters:" << std::endl;
