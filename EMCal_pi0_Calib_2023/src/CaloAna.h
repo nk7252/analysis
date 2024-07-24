3b269b3be27755ac409384df75b51f72f94386c9
Nikhil Kumar <137448289+nk7252@users.noreply.github.com>
2024-07-24 15:57:43 -0400

    Update CaloAna.h

--- EMCal_pi0_Calib_2023/src/CaloAna.h
+++ EMCal_pi0_Calib_2023/src/CaloAna.h
@@ -207,7 +207,8 @@ class CaloAna : public SubsysReco
   bool matchspmctruth = true;//SPMC. check must be in primary range
   bool additionalsmearing = true;//should be on for spmc in all cases. if you want 0 use 0 for smearint
   float badcalibsmear;
-  int badcalibsmearint=45;//thousandths. note that if pos corr is on I found 130 to be right. without I found 125.
+  //NEVER USE LEADING ZEROS IN DECIMALS. IT WILL BE INTERPRETED AS OCTAL
+  int badcalibsmearint=5;//thousandths. note that if pos corr is on I found 130 to be right. without I found 125.
   //gen MC: pythia
   bool matchmctruth = false;//NOT spmc. check must be in secondary range
 
