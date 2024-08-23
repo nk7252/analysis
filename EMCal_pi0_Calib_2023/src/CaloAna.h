6959dc1808e5c7afe58d9ffbef2bf0ae0ffa3c1f
Nikhil Kumar <137448289+nk7252@users.noreply.github.com>
2024-08-22 22:13:03 -0400

    Update CaloAna.h

--- EMCal_pi0_Calib_2023/src/CaloAna.h
+++ EMCal_pi0_Calib_2023/src/CaloAna.h
@@ -219,7 +219,7 @@ class CaloAna : public SubsysReco
   //NEVER USE LEADING ZEROS IN DECIMALS. IT WILL BE INTERPRETED AS OCTAL
   int badcalibsmearint=0;//thousandths. note that if pos corr is on I found 130 to be right. without I found 125.
   //gen MC: pythia, *should remove smeared/weighted histograms.*
-  bool matchmctruth = true;//NOT spmc. check must be in secondary range
+  bool matchmctruth = true;//NOT spmc. redundant with matchspmctruth
   bool filltruthspectrum = true;
 
   TH1F* h_pt1;
