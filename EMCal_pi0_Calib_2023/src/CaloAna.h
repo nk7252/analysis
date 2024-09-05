ac5cec128528169bd4aa4d92bc40f72b2c73fcf9
Nikhil Kumar <nkumar7252@gmail.com>
2024-09-05 14:19:32 -0400

    Update CaloAna.h

--- EMCal_pi0_Calib_2023/src/CaloAna.h
+++ EMCal_pi0_Calib_2023/src/CaloAna.h
@@ -217,7 +217,7 @@ class CaloAna : public SubsysReco
   float badcalibsmear;
   bool eta_weight = true;
   //NEVER USE LEADING ZEROS IN DECIMALS. IT WILL BE INTERPRETED AS OCTAL
-  int badcalibsmearint=0;//thousandths. note that if pos corr is on I found 130 to be right. without I found 125.
+  int badcalibsmearint=125;//thousandths. note that if pos corr is on I found 130 to be right. without I found 125.
   //gen MC: pythia, *should remove smeared/weighted histograms.*
   bool matchmctruth = true;//
   bool filltruthspectrum = true;
