98f42b731d0e47cc8767080aff26b101a68fdfc2
Nikhil Kumar <137448289+nk7252@users.noreply.github.com>
2024-09-20 16:19:03 -0400

    Update CaloAna.cc

--- EMCal_pi0_Calib_2023/src/CaloAna.cc
+++ EMCal_pi0_Calib_2023/src/CaloAna.cc
@@ -115,7 +115,7 @@ int CaloAna::Init(PHCompositeNode*)
   h_reco_etaphi = new TH2F("h_reco_eta", "Reco eta", 140, -1.2, 1.2, 64, -1 * TMath::Pi(), TMath::Pi());
   h_vtxmap_fail = new TH1F("h_vtxmap_fail", "Vtxmap Fail", 2, 0, 2);
   h_zvtx = new TH1F("h_zvtx", "Zvtx", 300, -150, 150);
-  for(int i = 0; i < 13; i++)
+  for(int i = 0; i < 15; i++)
   {
     h_reco_etaphi_cuts[i] = new TH2F(Form("h_reco_etaphi_cuts%d", i), "", 140, -1.2, 1.2, 64, -1 * TMath::Pi(), TMath::Pi());
     h_cluster_etaphi_cuts[i] = new TH2F(Form("h_cluster_etaphi_cuts%d", i), "", 140, -1.2, 1.2, 64, -1 * TMath::Pi(), TMath::Pi());
