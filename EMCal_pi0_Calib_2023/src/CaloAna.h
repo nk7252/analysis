f9e3f6462a995f6a065f51debf36d7cffc5c252c
Nikhil Kumar <nkumar7252@gmail.com>
2024-10-29 11:46:00 -0400

    Update CaloAna.h

--- EMCal_pi0_Calib_2023/src/CaloAna.h
+++ EMCal_pi0_Calib_2023/src/CaloAna.h
@@ -206,7 +206,7 @@ class CaloAna : public SubsysReco
   bool SPMC_bool = true;
   float badcalibsmear;
   bool Pythia_weight = false;
-  bool eta_weight = false;
+  bool eta_weight = true;
   // NEVER USE LEADING ZEROS IN DECIMALS. IT WILL BE INTERPRETED AS OCTAL
   bool additionalsmearing = true;  // should be on for spmc in all cases. if you want 0 use 0 for smearint
   int badcalibsmearint = 125;      // thousandths. note that if pos corr is on I found 130 to be right. without I found 125.
