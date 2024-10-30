a03597a33892005cbfdd5d7a3812fc9e4e444c92
Nikhil Kumar <nkumar7252@gmail.com>
2024-10-30 10:42:52 -0400

    Update CaloAna.cc

--- EMCal_pi0_Calib_2023/src/CaloAna.cc
+++ EMCal_pi0_Calib_2023/src/CaloAna.cc
@@ -310,7 +310,7 @@ int CaloAna::Init(PHCompositeNode*)
   // towers node selection
   calotowerinfostring = (clust_waveform == true) ? "WAVEFORM_CEMC" : "TOWERINFO_CALIB_CEMC";
   // clustercontainer node selection
-  clustcontainerstring = (poscor == true) ? "CLUSTER_POS_COR_CEMC" : ((recluster == true && SPMC_bool == false) ? "CLUSTERINFO_CEMC2" : ((clust_waveform == true) ? "CLUSTERINFO_CEMC" : "CLUSTER_CEMC"));
+  clustcontainerstring = (poscor == true) ? "CLUSTER_POS_COR_CEMC" : ((recluster == true) ? "CLUSTERINFO_CEMC2" : ((clust_waveform == true) ? "CLUSTERINFO_CEMC" : "CLUSTER_CEMC"));
   std::cout << "clustcontainerstring: " << clustcontainerstring << std::endl;
   std::cout << "calotowerinfostring: " << calotowerinfostring << std::endl;
 
