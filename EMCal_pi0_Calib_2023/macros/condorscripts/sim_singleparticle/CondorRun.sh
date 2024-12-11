3ae9a755a2f52b570d54f8c46375d44a2f85e2e6
Nikhil Kumar <nkumar7252@gmail.com>
2024-12-10 22:50:59 -0500

    add a new folder for spmc without full reco

---
+++ EMCal_pi0_Calib_2023/macros/condorscripts/sim_singleparticle/CondorRun.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source /sphenix/u/nkumar/setup.sh
+
+echo "------------------setting up environment--------------------"
+export Cur_dir=$(pwd)
+echo "running area:" ${Cur_dir}
+echo "-------------------------running----------------------------"
+cd ${Cur_dir}
+ls
+root '' > notes.log
+
+
+#root -b "Fun4All_G4_Waveform.C(0,\"inputdatahits.txt\")"
+root -b "Fun4All_G4_Waveform.C(0,\"inputdata.txt\",\"inputdatahits.txt\")"
+
+echo "JOB COMPLETE!"
